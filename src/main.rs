e588796f7079260c5a446890f4c7c4c5345f191c
marceline-cramer <cramermarceline@gmail.com>
2025-06-24 15:12:17 -0600

    Minor code cleanup and configurable orbits

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,85 @@
 use std::{f32::consts::TAU, fs::File};
 
-use glam::{DVec2, dvec2};
+use glam::DVec2;
 use raqote::{DrawOptions, DrawTarget, PathBuilder, SolidSource, Transform};
 use rayon::{
     iter::{ParallelBridge, ParallelIterator},
     slice::ParallelSlice,
 };
 use rustfft::{FftPlanner, num_complex::Complex64};
+use serde::{Deserialize, Serialize};
 
 fn main() {
-    let orbit = Orbit {
-        period: 6.325897,
-        initial_conds: vec![
-            Body {
-                mass: 1.0,
-                position: dvec2(-1.0, 0.0),
-                velocity: dvec2(0.347113, 0.532727),
-                color: SolidSource::from_unpremultiplied_argb(255, 255, 0, 0),
-            },
-            Body {
-                mass: 1.0,
-                position: dvec2(1.0, 0.0),
-                velocity: dvec2(0.347113, 0.532727),
-                color: SolidSource::from_unpremultiplied_argb(255, 0, 255, 0),
-            },
-            Body {
-                mass: 1.0,
-                position: dvec2(0.0, 0.0),
-                velocity: dvec2(-0.694226, -1.065454),
-                color: SolidSource::from_unpremultiplied_argb(255, 0, 0, 255),
-            },
-        ],
+    let config: Config = toml::from_str(&std::fs::read_to_string("orbits.toml").unwrap()).unwrap();
+
+    // hard-coded orbit from the configuration for now
+    let orbit = config.orbit[2].to_orbit();
+
+    let sim_config = SimulationConfig {
+        frames: 140,
+        subframes: 100,
     };
 
-    let config = SimulationConfig {
-        frames: 100,
-        subframes: 10,
-    };
-
-    let simulated = simulate_closed(&config, &orbit);
+    let simulated = simulate_closed(&sim_config, &orbit);
 
     let mut freqs = analyze(&simulated);
-    freqs.iter_mut().for_each(|freqs| optimize(0.01, freqs));
-    eprintln!("{freqs:#?}");
+    freqs.iter_mut().for_each(|freqs| optimize(0.001, freqs));
 
-    let by_body = invert(&simulated, Clone::clone);
+    let by_body = transpose(&simulated, Clone::clone);
 
     let total_frames = simulated.len();
     let mut compressed = Vec::new();
     for (body_freqs, baseline) in freqs.iter().zip(by_body.iter()) {
         let positions = inverse_analyze(total_frames, body_freqs);
-        println!("{}", rms_error(&positions, baseline));
+        println!("optimization error: {}", rms_error(&positions, baseline));
         compressed.push(positions);
     }
 
-    let compressed = invert(&compressed, Clone::clone);
-    render(&config, &orbit, &compressed);
+    let compressed = transpose(&compressed, Clone::clone);
+    render(&sim_config, &orbit, &compressed);
 }
 
-pub fn invert<T, O: Clone>(positions: &[Vec<T>], map: impl Fn(&T) -> O) -> Vec<Vec<O>> {
-    let mut by_body = vec![Vec::new(); positions[0].len()];
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Config {
+    pub orbit: Vec<OrbitConfig>,
+}
 
-    for frame in positions.iter() {
-        for (by_body, position) in by_body.iter_mut().zip(frame.iter()) {
-            by_body.push(map(position));
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct OrbitConfig {
+    pub name: String,
+    pub period: f64,
+    pub masses: Vec<f64>,
+    pub positions: Vec<DVec2>,
+    pub velocities: Vec<DVec2>,
+}
+
+impl OrbitConfig {
+    pub fn to_orbit(&self) -> Orbit {
+        let colors = [
+            SolidSource::from_unpremultiplied_argb(255, 255, 0, 0),
+            SolidSource::from_unpremultiplied_argb(255, 0, 255, 0),
+            SolidSource::from_unpremultiplied_argb(255, 0, 0, 255),
+        ];
+
+        let bodies = self
+            .masses
+            .iter()
+            .zip(self.positions.iter())
+            .zip(self.velocities.iter())
+            .zip(colors.iter().cycle())
+            .map(|(((mass, position), velocity), color)| Body {
+                mass: *mass,
+                color: *color,
+                position: *position,
+                velocity: *velocity,
+            })
+            .collect();
+
+        Orbit {
+            initial_conds: bodies,
+            period: self.period,
         }
     }
-
-    by_body
 }
 
 pub fn analyze(positions: &[Vec<DVec2>]) -> Vec<Vec<FrequencyComponent>> {
@@ -75,7 +87,7 @@ pub fn analyze(positions: &[Vec<DVec2>]) -> Vec<Vec<FrequencyComponent>> {
     let frame_num = positions.len();
     let fft = planner.plan_fft_forward(frame_num);
 
-    let mut freqs = invert(positions, |pos| Complex64 {
+    let mut freqs = transpose(positions, |pos| Complex64 {
         re: pos.x,
         im: pos.y,
     });
@@ -116,9 +128,9 @@ pub fn fft_to_freq(idx: usize, fft: Complex64, frame_num: usize) -> FrequencyCom
 }
 
 pub fn optimize(cutoff: f64, freqs: &mut Vec<FrequencyComponent>) {
-    let dc = freqs.iter().find(|freq| freq.freq.abs() < 0.001).cloned();
-    freqs.retain(|freq| freq.amplitude > cutoff);
-    freqs.extend(dc);
+    let original_length = freqs.len();
+    freqs.retain(|freq| freq.amplitude > cutoff || freq.freq.abs() < 0.01);
+    println!("optimized #freqs from {original_length} to {}", freqs.len());
 }
 
 pub fn inverse_analyze(frames: usize, freqs: &[FrequencyComponent]) -> Vec<DVec2> {
@@ -128,13 +140,6 @@ pub fn inverse_analyze(frames: usize, freqs: &[FrequencyComponent]) -> Vec<DVec2
             freqs.iter().map(|freq| freq.sample(sample)).sum()
         })
         .collect()
-}
-
-pub fn rms_error(lhs: &[DVec2], rhs: &[DVec2]) -> f64 {
-    lhs.iter()
-        .zip(rhs.iter())
-        .map(|(lhs_pos, rhs_pos)| lhs_pos.distance_squared(*rhs_pos) / lhs.len() as f64)
-        .sum()
 }
 
 #[derive(Clone, Debug)]
@@ -157,28 +162,107 @@ impl FrequencyComponent {
     }
 }
 
+pub fn simulate_closed(config: &SimulationConfig, orbit: &Orbit) -> Vec<Vec<DVec2>> {
+    let mut reversed = orbit.clone();
+
+    for body in reversed.initial_conds.iter_mut() {
+        body.velocity = -body.velocity;
+    }
+
+    let (mut forwards, mut backwards) =
+        rayon::join(|| simulate(config, orbit), || simulate(config, &reversed));
+
+    backwards.reverse();
+
+    // simulations should end where they started
+    // remove the last element to even out the period
+    forwards.pop();
+    backwards.pop();
+
+    let forwards_error = transpose(&forwards, Clone::clone);
+    let backwards_error = transpose(&backwards, Clone::clone);
+
+    for (forwards, backwards) in forwards_error.iter().zip(backwards_error.iter()) {
+        let error = rms_error(forwards, backwards);
+        println!("closed simulation RMS error: {error}",);
+        assert!(error < 0.001);
+    }
+
+    let frame_num = forwards.len();
+    for (idx, (forwards, backwards)) in forwards.iter_mut().zip(backwards.iter()).enumerate() {
+        let position = (idx as f64) / (frame_num as f64);
+        let blend = position;
+
+        for (forward, backward) in forwards.iter_mut().zip(backwards.iter()) {
+            *forward = forward.lerp(*backward, blend);
+        }
+    }
+
+    forwards
+}
+
+pub fn simulate(config: &SimulationConfig, orbit: &Orbit) -> Vec<Vec<DVec2>> {
+    let frame_num = config.frames * config.subframes;
+    let timestep = orbit.period / frame_num as f64;
+    let mut bodies = orbit.initial_conds.clone();
+    let mut history = Vec::with_capacity(frame_num);
+
+    let first: Vec<_> = bodies.iter().map(|body| body.position).collect();
+    history.push(first.clone());
+
+    let mut last = vec![];
+    for _ in 0..frame_num {
+        for _ in 0..10000 {
+            step(timestep / 10000.0, &mut bodies);
+        }
+
+        last = bodies.iter().map(|body| body.position).collect();
+        history.push(last.clone())
+    }
+
+    println!("start-end simulation drift: {}", rms_error(&first, &last));
+
+    history
+}
+
+pub fn rms_error(lhs: &[DVec2], rhs: &[DVec2]) -> f64 {
+    lhs.iter()
+        .zip(rhs.iter())
+        .map(|(lhs_pos, rhs_pos)| lhs_pos.distance_squared(*rhs_pos) / lhs.len() as f64)
+        .sum()
+}
+
+pub fn transpose<T, O: Clone>(positions: &[Vec<T>], map: impl Fn(&T) -> O) -> Vec<Vec<O>> {
+    let mut by_body = vec![Vec::new(); positions[0].len()];
+
+    for frame in positions.iter() {
+        for (by_body, position) in by_body.iter_mut().zip(frame.iter()) {
+            by_body.push(map(position));
+        }
+    }
+
+    by_body
+}
+
 pub fn render(config: &SimulationConfig, orbit: &Orbit, positions: &[Vec<DVec2>]) {
-    let subalpha = (255 / config.subframes / 2) as u8;
     let width = 400;
     let height = 400;
 
     let frames: Vec<_> = positions
-        .chunks(config.subframes)
-        .collect::<Vec<_>>()
-        .par_windows(2)
-        .map(|frames| {
+        .par_chunks(config.subframes)
+        .map(|frame| {
             let mut dt = DrawTarget::new(width as _, height as _);
-            dt.set_transform(&Transform::scale(100.0, 100.0).then_translate((200.0, 200.0).into()));
+            dt.clear(SolidSource::from_unpremultiplied_argb(255, 100, 100, 100));
+            dt.set_transform(&Transform::scale(50.0, 50.0).then_translate((200.0, 200.0).into()));
 
-            for frame in frames.iter() {
-                for subframe in frame.iter() {
-                    draw(&mut dt, &orbit.initial_conds, subframe, subalpha);
-                }
+            let step = frame.len().div_ceil(10);
+            for subframe in frame.iter().step_by(step) {
+                draw(&mut dt, &orbit.initial_conds, subframe);
             }
 
             let mut frame = gif::Frame::from_rgba(width, height, dt.get_data_u8_mut());
             frame.delay = 2;
-            frame.dispose = gif::DisposalMethod::Background;
+            frame.dispose = gif::DisposalMethod::Keep;
             frame.make_lzw_pre_encoded();
 
             frame
@@ -194,47 +278,6 @@ pub fn render(config: &SimulationConfig, orbit: &Orbit, positions: &[Vec<DVec2>]
     }
 }
 
-pub fn simulate_closed(config: &SimulationConfig, orbit: &Orbit) -> Vec<Vec<DVec2>> {
-    let mut reversed = orbit.clone();
-
-    for body in reversed.initial_conds.iter_mut() {
-        body.velocity = -body.velocity;
-    }
-
-    let (mut forwards, backwards) =
-        rayon::join(|| simulate(config, orbit), || simulate(config, &reversed));
-
-    let frame_num = forwards.len();
-    for (idx, frame) in forwards.iter_mut().enumerate() {
-        let position = (idx as f64) / (frame_num as f64);
-        let blend = position;
-
-        let backwards_idx = frame_num - idx - 1;
-        let backwards_frame = &backwards[backwards_idx];
-
-        for (forward, backward) in frame.iter_mut().zip(backwards_frame.iter()) {
-            *forward = forward.lerp(*backward, blend);
-        }
-    }
-
-    forwards
-}
-
-pub fn simulate(config: &SimulationConfig, orbit: &Orbit) -> Vec<Vec<DVec2>> {
-    let timestep = orbit.period / (config.frames * config.subframes) as f64;
-    let mut bodies = orbit.initial_conds.clone();
-    let mut history = vec![bodies.iter().map(|body| body.position).collect()];
-
-    for _ in 0..config.frames {
-        for _ in 0..config.subframes {
-            step(timestep, &mut bodies);
-            history.push(bodies.iter().map(|body| body.position).collect());
-        }
-    }
-
-    history
-}
-
 #[derive(Clone, Debug)]
 pub struct SimulationConfig {
     pub frames: usize,
@@ -247,27 +290,13 @@ pub struct Orbit {
     pub period: f64,
 }
 
-#[derive(Clone, Debug)]
-pub struct Body {
-    pub mass: f64,
-    pub color: SolidSource,
-    pub position: DVec2,
-    pub velocity: DVec2,
-}
-
-impl Body {
-    pub fn apply_force(&mut self, dt: f64, force: DVec2) {
-        self.velocity += dt * force * self.mass;
-    }
-}
-
-pub fn draw(dt: &mut DrawTarget, bodies: &[Body], positions: &[DVec2], a: u8) {
+pub fn draw(dt: &mut DrawTarget, bodies: &[Body], positions: &[DVec2]) {
     for (body, position) in bodies.iter().zip(positions.iter()) {
         let mut pb = PathBuilder::new();
         pb.arc(position.x as f32, position.y as f32, 0.08, 0.0, TAU);
 
         let color =
-            SolidSource::from_unpremultiplied_argb(a, body.color.r, body.color.g, body.color.b);
+            SolidSource::from_unpremultiplied_argb(13, body.color.r, body.color.g, body.color.b);
 
         let path = pb.finish();
         let src = raqote::Source::Solid(color);
@@ -294,8 +323,8 @@ pub fn apply_forces(dt: f64, bodies: &mut [Body]) {
             let force = mass / r2;
 
             let delta = delta.normalize();
-            body.apply_force(dt, force * -delta);
-            other.apply_force(dt, force * delta);
+            body.velocity -= dt * force * delta;
+            other.velocity += dt * force * delta;
         }
     }
 }
@@ -304,4 +333,12 @@ pub fn update(dt: f64, bodies: &mut [Body]) {
     for body in bodies.iter_mut() {
         body.position += body.velocity * dt;
     }
+}
+
+#[derive(Clone, Debug)]
+pub struct Body {
+    pub mass: f64,
+    pub color: SolidSource,
+    pub position: DVec2,
+    pub velocity: DVec2,
 }
